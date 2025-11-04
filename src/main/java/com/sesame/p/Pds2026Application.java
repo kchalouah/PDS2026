87f67e44215c7959e737b7c1c6cc2da5f4d1e6a2
Kchalouah <100380797+kchalouah@users.noreply.github.com>
2025-11-04 11:06:12 +0100

    -project created-

---
+++ src/main/java/com/sesame/p/Pds2026Application.java
@@ -0,0 +1,13 @@
+package com.sesame.p;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pds2026Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pds2026Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sesame/p/Pds2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sesame.p;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pds2026ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
